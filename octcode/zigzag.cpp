b881cbab5737c1d7f34fc9c091b361a45f42c77e
Vishal Raj <9031842908.vk@gmail.com>
2023-03-21 18:34:04 +0530

    Create zigzag.cpp

---
+++ octcode/zigzag.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=9;j++){
+            if((i+j)%4==0){
+                cout<<"*";
+            }
+            else if(i==2 && j%4==0){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
