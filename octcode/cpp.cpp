54ca4a4080ef574f40692473bdea3d46a69abd29
Vishal Raj <9031842908.vk@gmail.com>
2023-04-08 00:25:42 +0530

    Create cpp.cpp

---
+++ octcode/cpp.cpp
@@ -0,0 +1,21 @@
+Given a string, your task is to reorder its letters in such a way that it becomes a palindrome (i.e., it reads the same forwards and backwards).
+
+Input
+
+The only input line has a string of length n
+ consisting of characters A–Z.
+
+Output
+
+Print a palindrome consisting of the characters of the original string. You may print any valid solution. If there are no solutions, print "NO SOLUTION".
+
+Constraints
+1≤n≤106
+
+Example
+
+Input:
+AAAACACBA
+
+Output:
+AACABACAA
