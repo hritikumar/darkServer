871b651afda9086afa613dbe1b161299ee47e5a5
Vishal Raj <9031842908.vk@gmail.com>
2023-04-30 14:22:05 +0530

    Create k.cpp

---
+++ octcode/k.cpp
@@ -0,0 +1,5 @@
+4. Which of the following statements are true for Stochastic Gradient Descent (SGD)?
+The model weights are updated after going over the full training data in each iteration
+The model weights are updated for every single training data point in each iteration
+SGD works better than batch GD for small datasets
+Convergence path of SGD is less noisy compared to batch GD
